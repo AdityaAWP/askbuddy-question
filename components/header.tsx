0ffffeb004b3f08d2663f82e07c1a69fc3c27305
AdityaAWP <aditwpratama1@gmail.com>
2025-05-14 00:10:48 +0700

    Small Changes

--- components/header.tsx
+++ components/header.tsx
@@ -27,13 +27,10 @@ const Header = () => {
         <div className="hidden md:flex items-center gap-6">
           <div className="flex gap-6">
             <Link href="/create-room" className="hover:text-gray-600 transition-colors">
-              <h1>Create Room</h1>
+              <h1>Buat Room</h1>
             </Link>
             <Link href="/solo" className="hover:text-gray-600 transition-colors">
-              <h1>Solo Mode</h1>
-            </Link>
-            <Link href="#" className="hover:text-gray-600 transition-colors">
-              <h1>Instruction</h1>
+              <h1>Mode Solo</h1>
             </Link>
           </div>
         </div>
